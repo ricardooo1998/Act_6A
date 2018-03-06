fa7097aac2fa1c60be7aa98a91baf4d0fbc8143e
alumno <alumno@pc-alumno.(none)>
2018-03-06 16:27:34 +0100

    Subimos ejercicio 1

---
+++ ejer1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+num1=10
+num2=15
+
+if num1 -eq num2
+echo "Los dos números introducidos son iguales"
+
+if num1 -gt num2
+    then echo "$num1 es mayor que $num2"
+else
+    then echo "$num2 es mayor que $num1"
